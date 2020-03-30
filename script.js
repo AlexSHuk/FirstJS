514b1ed6d88122a5314a95708a5404f5a1f21703
AlexHuk <alexshokarev@gmail.com>
2020-03-30 16:17:23 +0300

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'use strict';
 var money = +prompt("Ваш бюджет на месяц?") ;
-var time = prompt("Введите дату в формате YYYY-MM-DD" , "YYYY-MM-DD");
-var firstAnswer = prompt("Введите обязательную статью расходов в этом месяце");
-var secondAnswer = +prompt( "Во сколько обойдется?");
+let time = prompt("Введите дату в формате YYYY-MM-DD" , "YYYY-MM-DD");
+
 
 var appData = {
 cash : money,
@@ -13,9 +12,53 @@ income : [],
 savings: false
 };
 
-appData.expenses[firstAnswer] = secondAnswer;
+// цикл FOR
 
-var budget = (money - secondAnswer)/30;
+// for (let i=0; i<2; i++) {
+// 	let firstAnswer = prompt("Введите обязательную статью расходов в этом месяце"),
+// 	secondAnswer = +prompt( "Во сколько обойдется?");
+
+// 	if (typeof(firstAnswer)==='string' && (typeof(firstAnswer) != null ) && (typeof(secondAnswer) != null ) 
+// 		&& firstAnswer != '' && secondAnswer != '' && firstAnswer.length < 25) {
+
+// 		appData.expenses[firstAnswer] = secondAnswer;
+// 	} else i-- ;
+
+// }
+
+// цикл while
+
+// let i = 0;
+// while (i<2) {
+// 	let firstAnswer = prompt("Введите обязательную статью расходов в этом месяце"),
+// 	secondAnswer = +prompt( "Во сколько обойдется?");
+
+// 	if (typeof(firstAnswer)==='string' && (typeof(firstAnswer) != null ) && (typeof(secondAnswer) != null ) 
+// 		&& firstAnswer != '' && secondAnswer != '' && firstAnswer.length < 25) {
+
+// 		appData.expenses[firstAnswer] = secondAnswer;
+// 		i++;
+// 	} else i = i;
+// }
+
+// цикл DO
+let i=0;
+do {
+	let firstAnswer = prompt("Введите обязательную статью расходов в этом месяце"),
+	secondAnswer = +prompt( "Во сколько обойдется?");
+
+	if (typeof(firstAnswer)==='string' && (typeof(firstAnswer) != null ) && (typeof(secondAnswer) != null ) 
+		&& firstAnswer != '' && secondAnswer != '' && firstAnswer.length < 25) {
+
+		appData.expenses[firstAnswer] = secondAnswer;
+	i++;
+	} 
+}
+
+while (i<2);
+
+
+let budget = money/30;
 alert("Ваш бюджет на 1 день составляет " + budget + " грн");
 
 console.log(appData.cash);
